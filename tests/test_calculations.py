6a0ba58b37841672e658a34caa7437edd1525d64
Markus Dahl Jørgensen <markus.dahl.jorgensen@gmail.com>
2025-06-30 12:56:28 +0000

    Implemented gardners equation

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,7 @@
 
 from geo_calculator.calculations import find_average
+from geo_calculator.calculations import gardners_equation
+import pytest
 
 def test_find_average() -> None:
     test_list = [1,2,3,4,5]
@@ -11,3 +13,10 @@ def test_find_average() -> None:
 def test_length_of_string() -> None:
     test_string = "python"
     assert len(test_string) == 6
+
+def test_gardners_equation():
+    velocity = 2000  # m/s
+    expected_density = 2.0730949  # g/cm3
+
+    # By default, approx considers numbers within a relative tolerance of 1e-6
+    assert gardners_equation(velocity) == pytest.approx(expected_density)
