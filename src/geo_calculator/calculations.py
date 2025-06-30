8bb24a99f1d680e6a22a9481a3c15544291e287f
Markus Dahl Jørgensen <markus.dahl.jorgensen@gmail.com>
2025-06-30 13:05:51 +0000

    Added docstring for gardners equation

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -3,4 +3,10 @@ def find_average(list):
     return sum(list)/len(list)
 
 def gardners_equation(velocity):
+    """Calculate the bulk density of a waves litheology
+    Args:
+        Velocity: Seismic P-wave velcity
+    Returns:
+        Bulk density of the waves litheology based on velocity
+    """
     return 0.31*velocity**(0.25)
